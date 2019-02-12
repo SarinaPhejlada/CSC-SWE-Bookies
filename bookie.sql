e614c479aba940703b590b1398189c05b21e52d5
Tien Tran <tientran@Tiens-MacBook-Pro.local>
2019-02-12 06:44:52 -0500

    updated database

--- bookie.sql
+++ bookie.sql
@@ -3,13 +3,32 @@ CREATE DATABASE IF NOT EXISTS book_review;
 USE book_review;
 
 DROP TABLE IF EXISTS book,
-					 bood_id;
+					 bood_id,
+                     users,
+                     customerInfo;
+                     
+CREATE TABLE users (
+    userName	varchar(50)   	NOT NULL,
+    pass		varchar(50)		Not null,	
+    PRIMARY KEY (userName)
+);
+
+
 CREATE TABLE book (
     ISBN	int(50)   	unique NOT NULL,
     book_title		varchar(50)		Not null,	
 	publish_date int(50)   	NOT NULL,
     star_review		int(50)		Not null,
     PRIMARY KEY (ISBN)
+);
+
+CREATE TABLE customerInfo (
+    userName	varchar(24)   	unique		NOT NULL,
+    fName   	varchar(12) 	not null,
+    lName		varchar(12) 	not null,
+    birthDay	date			not null,
+    foreign key (userName) references users (userName),
+    PRIMARY KEY (userName)
 );
 
 CREATE TABLE book_id (
@@ -22,5 +41,5 @@ CREATE TABLE book_id (
 	NPR		varchar(100) 	not null,
     foreign key (ISBN) references book (ISBN),
     PRIMARY KEY (book_id)
-    /*UNIQUE  KEY (dept_name)*/
+
 );
